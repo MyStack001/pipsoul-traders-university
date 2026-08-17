1cb060114e40a0f73eab057713066dc58c159f21
obaahshadrach <obaahshadrach@gmail.com>
2026-08-17 13:39:06 +0100

    Add Home route with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Courses from "./components/Courses";
-import Roadmap from "./components/Roadmap";
-import Mentorship from "./components/Mentorship";
-import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-950 text-white">
-      <Navbar />
-
-     <main className="pt-20">
-  <Hero />
-  <Courses />
-  <Roadmap />
-  <Mentorship />
-</main>
-<Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Courses from "../components/Courses";
+import Roadmap from "../components/Roadmap";
+import Mentorship from "../components/Mentorship";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white">
+      <Navbar />
+
+      <main className="pt-20">
+        <Hero />
+        <Courses />
+        <Roadmap />
+        <Mentorship />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
